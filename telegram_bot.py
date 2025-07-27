305665b0064a27487d705cdac0f411d631913aec
thiennguyenhieu <thien.nguyenhieu@gmail.com>
2025-07-27 21:17:13 +0700

    fix parsing error

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,6 +8,6 @@ bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 def send_telegram_message(text: str):
     try:
-        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text, parse_mode='HTML', disable_web_page_preview=True)
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text, disable_web_page_preview=True)
     except Exception as e:
         print(f"[Telegram] Failed to send message: {e}")
